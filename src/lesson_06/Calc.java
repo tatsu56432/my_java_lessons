dae4d3db7c34ef9fe6c70274278e21df4ebd43cb
t_nakajima <t_nakajima@bbmedia.co.jp>
2018-05-21 12:42:26 +0900

    first commit

---
+++ src/lesson_06/Calc.java
@@ -0,0 +1,17 @@
+package lesson_06;
+
+public class Calc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 10; int b =2;
+		
+		int total = CalcLogic.tasu(a,b);
+		int delta = CalcLogic.hiku(a,b);
+		
+		System.out.println("‘«‚·‚Æ" + total + ",ˆø‚­‚Æ" + delta);
+
+	}
+
+}
