befce5d4a324d756dfd050fbdf3705ca03e7fab5
t_nakajima <t_nakajima@bbmedia.co.jp>
2018-05-22 19:32:45 +0900

    add matango

--- src/lesson_08/Main.java
+++ src/lesson_08/Main.java
@@ -6,17 +6,23 @@ public class Main {
 		// TODO Auto-generated method stub
 		
 		Hero h = new Hero();
-		
 		h.name = "たつお";
 		h.hp = 100;
 		
-		System.out.println("勇者" + h.name + "を生み出しました!");
-		
-		h.sit(5);
-		h.slip();
-		h.sit(25);
-		h.run();
+		Matango m1 = new Matango();
+		m1.hp = 50;
+		m1.suffix = 'A';
 
+		Matango m2 = new Matango();
+		m2.hp = 50;
+		m2.suffix = 'B';
+		
+		//冒険の始まり
+		h.slip();
+		m1.run();
+		m2.run();
+
+		h.run();
 	}
 
 }
---
+++ src/lesson_08/Matango.java
@@ -0,0 +1,10 @@
+package lesson_08;
+
+public class Matango {
+	int hp;
+	final int level = 10;
+	char suffix;
+	void run() {
+	System.out.println("お化けキノコ" + this.suffix + "は逃げ出した。");	
+	}
+}
