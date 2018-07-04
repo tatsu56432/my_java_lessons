afec2a78a64d2aad6590639ead75b716961efe78
t_nakajima <t_nakajima@bbmedia.co.jp>
2018-07-04 23:50:12 +0900

    更新

--- src/lesson_12/Shape.java
+++ src/lesson_12/Shape.java
@@ -1,5 +1,11 @@
 package lesson_12;
 
-public class Shape {
+public abstract class Shape implements Figure{
+
+	@Override
+	public abstract void draw();
+
+	@Override
+	public abstract double perimeter();
 
 }
--- src/lesson_12/Triangle.java
+++ src/lesson_12/Triangle.java
@@ -6,29 +6,50 @@ public class Triangle extends Polygon {
 	private Point p2;
 	private Point p3;
 	
+	public Point getP1() {
+		return p1;
+	}
+
+	public Point getP2() {
+		return p2;
+	}
+
+	public Point getP3() {
+		return p3;
+	}
+
 	
-	public Triangle(int x1,int y1,int x2,int y2,int x3,int y3) {
-	
+	public Triangle(int x1,int y1,int x2,int y2,int x3,int y3) {	
 		
 		this.p1 = new Point(x1,y1);
 		this.p2 = new Point(x2,y2);
 		this.p3 = new Point(x3,y3);
 		
-		
-		
-		
 	}
 	
 	@Override
 	public void draw() {
-		// TODO Auto-generated method stub
-		
+		// TODO Auto-generated method stub		
+		Point point1 = this.getP1();
+		Point point2 = this.getP2();
+		Point point3 = this.getP3();		
+		System.out.println("三角形を描画"+"点1"+ point1.getX() + "," + point1.getY() + "から点2" + point2.getX() + "," + point2.getY() + ",点3"+ point3.getX() + "," + point3.getY()   +"の三角形");
 	}
 
 	@Override
 	public double perimeter() {
 		// TODO Auto-generated method stub
-		return 0;
+		Point point1 = this.getP1();
+		Point point2 = this.getP2();
+		Point point3 = this.getP3();
+		
+		int side_1 = (point1.getY() - point1.getY());
+		int side_2 = (point2.getY() - point2.getY());
+		int side_3 = (point3.getY() - point3.getY());
+		
+		double result = (side_1 + side_2 + side_3);
+		
+		return result;
 	}
 
 }
