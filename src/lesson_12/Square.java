8ecdb4d4afbc9afc280414c83618b8c0a0cdc6a0
tatsuo nakajima <tatsu56432@gmail.com>
2018-07-06 10:04:47 +0900

    commit

--- src/lesson_12/Square.java
+++ src/lesson_12/Square.java
@@ -2,8 +2,6 @@ package lesson_12;
 
 public class Square extends Rectangle{
 
-	
-	
 	public Square(int x, int y, int width) {
 		super(x, y, width, 0);
 		// TODO Auto-generated constructor stub
@@ -11,10 +9,18 @@ public class Square extends Rectangle{
 	
 	public void draw() {
 		
-		Poinnt p.getX();
-		System.out.println("正方刑を描画");
-		
-		
+		int point_x = p.getX();
+		int point_y = p.getY();
+		int width = this.width;
+		int height = this.height;
+		System.out.println("正方刑を描画" + point_x +"," + point_y +" を描画する"+ "幅高さは"+ width +"を描画する");
 	}
-
+	
+	public double perimeter() {
+		double result;
+		int width = this.width;
+		result = width *4;
+		
+		return result;
+	}
 }
